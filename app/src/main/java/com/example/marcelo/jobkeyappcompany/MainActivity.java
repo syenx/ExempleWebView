9383f8c04bd200c251596f75c76b9b47a11fa298
Marcelo Franco <redbull_70@hotmail.com>
2018-10-25 00:12:33 -0300

    first commit

---
+++ app/src/main/java/com/example/marcelo/jobkeyappcompany/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.marcelo.jobkeyappcompany;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+public class MainActivity extends AppCompatActivity {
+    private WebView myWebView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myWebView = (WebView) findViewById(R.id.webview);
+        WebViewClientImpl webViewClient = new WebViewClientImpl(MainActivity.this);
+        myWebView.setWebViewClient(webViewClient);
+
+        myWebView.setWebViewClient(new WebViewClient());
+
+
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        myWebView.loadUrl("http://jobkey.gear.host/ponto/loginp.aspx");
+
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && myWebView.canGoBack()) {
+            myWebView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
